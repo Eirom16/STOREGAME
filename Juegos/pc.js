b6592c16cd36f4128eefd7eb7496a7999e6faa7d
Eirom Urbaez <elizabethmatos983@gmail.com>
2025-05-18 18:38:46 -0400

    mas juegos

---
+++ Juegos/pc.js
@@ -0,0 +1,300 @@
+const juegosPC = {
+
+  blur: {
+  titulo: "Blur",
+  descripcion: "Blur es un videojuego de carreras arcade desarrollado por Bizarre Creations y publicado por Activision. Combina carreras de alta velocidad con poderosos ítems de combate, ofreciendo una experiencia similar a Mario Kart pero con autos realistas y gráficos detallados.",
+  imagen: "https://m.media-amazon.com/images/I/810xE2BD6bL._AC_UF1000,1000_QL80_.jpg",
+  enlace: "https://cuty.io/9QpMHd22c",
+  categoria: "Carreras",
+  trailer: "https://www.youtube.com/embed/yBDLMzwMAdc?si=kyS5FxBraMNtY_Yg",
+  platafortma: "PC", 
+  requisitos:  `
+    <strong>Requisitos mínimos</strong>
+    <ul>
+      <li>SO: Windows XP / Vista / 7</li>
+      <li>Procesador: Intel Pentium D 3.4GHz / AMD Athlon 64 4000+</li>
+      <li>RAM: 2 GB</li>
+      <li>Gráficos: GeForce 6600 GT / ATI Radeon 1600 XT (256 MB VRAM)</li>
+      <li>DirectX: Versión 9.0c</li>
+      <li>Almacenamiento: 14 GB de espacio disponible</li>
+    </ul>
+    <strong>Requisitos recomendados</strong>
+    <ul>
+      <li>SO: Windows XP / Vista / 7</li>
+      <li>Procesador: Intel Core 2 Duo E6400 / AMD Athlon 64 X2 5800+</li>
+      <li>RAM: 4 GB</li>
+      <li>Gráficos: GeForce 8800 GT / ATI Radeon HD 3870</li>
+      <li>DirectX: Versión 9.0c</li>
+      <li>Almacenamiento: 14 GB de espacio disponible</li>
+    </ul>
+  `
+},
+
+ dbz: {
+  titulo: "Dragon Ball: Sparking! Zero",
+  descripcion: "Dragon Ball: Sparking! Zero es un videojuego de lucha 3D desarrollado por Spike Chunsoft y publicado por Bandai Namco Entertainment. Es la continuación espiritual de la saga Budokai Tenkaichi, trayendo combates dinámicos y explosivos con una extensa plantilla de personajes del universo Dragon Ball.",
+  imagen: "https://www.gamestorrents.app/wp-content/uploads/2025/04/Dragon-Ball-Sparking-ZERO-pc-free-download.png",
+  enlace: "https://cuty.io/QE32rO5g",
+  categoria: "Lucha",
+  trailer: "https://www.youtube.com/embed/XTvBSY0YMyg?si=-7wfz8ZG-I9tSr8u",
+  platafortma: "PC",
+  requisitos:  `
+    <strong>Requisitos mínimos</strong>
+    <ul>
+      <li>Requiere un procesador y un sistema operativo de 64 bits</li>
+      <li>SO: Windows 10 (64 bits)</li>
+      <li>Procesador: Intel Core i5-9600K / AMD Ryzen 5 2600</li>
+      <li>Memoria: 8 GB de RAM</li>
+      <li>Gráficos: NVIDIA GeForce GTX 1660 Ti / AMD Radeon RX 590</li>
+      <li>DirectX: Versión 11</li>
+      <li>Almacenamiento: 50 GB de espacio disponible</li>
+    </ul>
+    <strong>Requisitos recomendados</strong>
+    <ul>
+      <li>SO: Windows 10 / Windows 11 (64 bits)</li>
+      <li>Procesador: Intel Core i7-9700K / AMD Ryzen 5 3600</li>
+      <li>Memoria: 16 GB de RAM</li>
+      <li>Gráficos: NVIDIA GeForce RTX 2070 / AMD Radeon RX 6700 XT</li>
+      <li>DirectX: Versión 12</li>
+      <li>Almacenamiento: 50 GB de espacio disponible</li>
+    </ul>
+  `
+},
+
+hollowKnight: {
+  titulo: "Hollow Knight",
+  descripcion: "Hollow Knight es un juego de acción y aventuras en 2D desarrollado por Team Cherry. Ambientado en un mundo subterráneo llamado Hallownest, el jugador explora vastas cavernas, combate enemigos y descubre secretos en una aventura estilo metroidvania con un estilo artístico dibujado a mano.",
+  imagen: "https://www.gamestorrents.app/wp-content/uploads/2018/04/HOLLOW-.jpg",
+  enlace: "https://cuty.io/wHyKIJ",
+  categoria: "Aventura",
+  trailer: "https://www.youtube.com/embed/nSPJXlYjENE?si=m2vh42v4mLAKoSSQ",
+  platafortma: "PC",
+  requisitos:  `
+    <strong>Requisitos mínimos</strong>
+    <ul>
+      <li>SO: Windows 7 (64-bit)</li>
+      <li>Procesador: Intel Core 2 Duo E5200</li>
+      <li>RAM: 4 GB</li>
+      <li>Gráficos: GeForce 9800GTX+ (1GB)</li>
+      <li>DirectX: Versión 10</li>
+      <li>Almacenamiento: 9 GB de espacio disponible</li>
+    </ul>
+    <strong>Requisitos recomendados</strong>
+    <ul>
+      <li>SO: Windows 10 (64-bit)</li>
+      <li>Procesador: Intel Core i5</li>
+      <li>RAM: 8 GB</li>
+      <li>Gráficos: GeForce GTX 560</li>
+      <li>DirectX: Versión 11</li>
+      <li>Almacenamiento: 9 GB de espacio disponible</li>
+    </ul>
+  `
+},
+
+sanAndreas: {
+  titulo: "Grand Theft Auto: San Andreas",
+  descripcion: "GTA: San Andreas es un videojuego de acción-aventura en mundo abierto desarrollado por Rockstar North. Ambientado en el estado ficticio de San Andreas, el juego sigue a Carl 'CJ' Johnson mientras regresa a su ciudad natal para investigar el asesinato de su madre y recuperar el control de su antigua banda.",
+  imagen: "https://www.gamestorrents.app/wp-content/uploads/2017/12/grand-theft-auto-.jpg",
+  enlace: "https://cuty.io/dgFiy8GdZD3",
+  categoria: "Acción",
+  trailer: "https://www.youtube.com/embed/yOzcbtsw_pQ?si=Y-xAmbQ0tP-CMdEr",
+  platafortma: "PC", 
+  requisitos:  `
+    <strong>Requisitos mínimos</strong>
+    <ul>
+      <li>SO: Windows XP / 2000</li>
+      <li>Procesador: 1 GHz Pentium III o AMD Athlon</li>
+      <li>RAM: 256 MB</li>
+      <li>Gráficos: 64 MB con soporte DirectX 9.0 (GeForce 3 o superior)</li>
+      <li>Almacenamiento: 3.6 GB de espacio libre</li>
+      <li>Sonido: Compatible con DirectX 9.0</li>
+    </ul>
+    <strong>Requisitos recomendados</strong>
+    <ul>
+      <li>SO: Windows XP / 2000</li>
+      <li>Procesador: Intel Pentium 4 o AMD Athlon XP</li>
+      <li>RAM: 384 MB o más</li>
+      <li>Gráficos: 128 MB (GeForce 6 o superior)</li>
+      <li>Almacenamiento: 4.7 GB de espacio libre</li>
+    </ul>
+  `
+},
+
+gtaV: {
+  titulo: "Grand Theft Auto V",
+  descripcion: "Grand Theft Auto V es un videojuego de acción y aventura desarrollado por Rockstar North. Ambientado en la ficticia ciudad de Los Santos, el juego presenta un mundo abierto donde los jugadores pueden explorar, realizar misiones, y participar en actividades delictivas. Ofrece una historia dinámica con tres protagonistas jugables.",
+  imagen: "https://www.gamestorrents.app/wp-content/uploads/2019/03/gta-v.jpg",
+  enlace: "https://cuty.io/YccOOJI",
+  categoria: "Acción",
+  trailer: "https://www.youtube.com/embed/QkkoHAzjnUs?si=z4RhvN0D0zzjWLYX",
+  platafortma: "PC",
+   requisitos:  `
+    <strong>Requisitos mínimos</strong>
+    <ul>
+      <li>SO: Windows 10 / 8.1 / 8 / 7 (64 bits)</li>
+      <li>Procesador: Intel Core i5-2500K / AMD FX-6300</li>
+      <li>RAM: 8 GB</li>
+      <li>Gráficos: NVIDIA GeForce GTX 760 2GB / AMD Radeon RX 660 2GB</li>
+      <li>DirectX: Versión 11</li>
+      <li>Almacenamiento: 65 GB de espacio disponible</li>
+    </ul>
+    <strong>Requisitos recomendados</strong>
+    <ul>
+      <li>SO: Windows 10 (64 bits)</li>
+      <li>Procesador: Intel Core i7-4770K / AMD Ryzen 5 1600</li>
+      <li>RAM: 16 GB</li>
+      <li>Gráficos: NVIDIA GeForce GTX 1060 6GB / AMD Radeon RX 580 4GB</li>
+      <li>DirectX: Versión 11</li>
+      <li>Almacenamiento: 65 GB de espacio disponible</li>
+    </ul>
+  `
+},
+
+
+cuphead: {
+  titulo: "Cuphead The Delicious Last Course",
+  descripcion: "Cuphead es un videojuego de acción y disparos tipo run and gun desarrollado por Studio MDHR. Inspirado en los dibujos animados de los años 30, se caracteriza por su estilo visual retro, música jazz y una jugabilidad centrada en desafiantes combates contra jefes.",
+  imagen: "https://www.gamestorrents.app/wp-content/uploads/2022/07/Cuphead-pc-free-download.jpg",
+  enlace: "https://cuty.io/zMV2BtGR5y",
+  categoria: "Aventura",
+  trailer: "https://www.youtube.com/embed/NN-9SQXoi50?si=Wtgvf7SX5UXZUDqx",
+  platafortma: "PC", 
+  requisitos:  `
+    <strong>Requisitos mínimos</strong>
+    <ul>
+      <li>SO: Windows 7</li>
+      <li>Procesador: Intel Core 2 Duo E8400</li>
+      <li>RAM: 3 GB</li>
+      <li>Gráficos: Intel HD Graphics 4000</li>
+      <li>DirectX: Versión 11</li>
+      <li>Almacenamiento: 4 GB de espacio disponible</li>
+    </ul>
+    <strong>Requisitos recomendados</strong>
+    <ul>
+      <li>SO: Windows 10</li>
+      <li>Procesador: Intel Core i3 o superior</li>
+      <li>RAM: 4 GB</li>
+      <li>Gráficos: NVIDIA GeForce GTX 660 o superior</li>
+      <li>DirectX: Versión 11</li>
+      <li>Almacenamiento: 4 GB de espacio disponible</li>
+    </ul>
+  `
+},
+
+celeste: {
+  titulo: "Celeste",
+  descripcion: "Celeste es un juego de plataformas desarrollado por Matt Makes Games. Controlas a Madeline en su ascenso a la montaña Celeste, enfrentando desafíos precisos y una historia emocional.",
+  imagen: "https://www.gamestorrents.app/wp-content/uploads/2018/02/Celeste.jpg",
+  enlace: "https://cuty.io/b0pZ3Yzl3Nf",
+  categoria: "Plataformas",
+  trailer: "https://www.youtube.com/embed/70d9irlxiB4?si=1H70X-jerz_GZFdu",
+  platafortma: "PC", 
+  requisitos:  `
+    <strong>Requisitos mínimos</strong>
+    <ul>
+      <li>SO: Windows 7</li>
+      <li>Procesador: Intel Core i3 M380</li>
+      <li>RAM: 2 GB</li>
+      <li>Gráficos: Intel HD 4000</li>
+      <li>Almacenamiento: 1.2 GB</li>
+    </ul>
+    <strong>Requisitos recomendados</strong>
+    <ul>
+      <li>SO: Windows 10</li>
+      <li>Procesador: Intel Core i5</li>
+      <li>RAM: 4 GB</li>
+      <li>Gráficos: GeForce GTX 560</li>
+      <li>Almacenamiento: 1.2 GB</li>
+    </ul>
+  `
+},
+
+doomEternal: {
+  titulo: "DOOM Eternal",
+  descripcion: "DOOM Eternal es un shooter en primera persona desarrollado por id Software. Retoma la lucha del Slayer contra hordas demoníacas con acción rápida y brutal.",
+  imagen: "https://www.gamestorrents.app/wp-content/uploads/2023/09/DOOM-Eternal-pc-free-download.jpg",
+  enlace: "https://cuty.io/c100do7",
+  trailer: "https://www.youtube.com/embed/6SRj82vc1Jg?si=NeMLsW8hi4NVbUM2",
+  platafortma: "PC",
+  requisitos:  `
+    <strong>Requisitos mínimos</strong>
+    <ul>
+      <li>SO: Windows 10 64-bit</li>
+      <li>Procesador: Intel Core i5-4430 / AMD FX-8350</li>
+      <li>RAM: 8 GB</li>
+      <li>Gráficos: NVIDIA GTX 1050Ti / AMD RX 470</li>
+      <li>Almacenamiento: 50 GB</li>
+    </ul>
+    <strong>Requisitos recomendados</strong>
+    <ul>
+      <li>Procesador: Intel Core i7-6700K / AMD Ryzen 7 1800X</li>
+      <li>RAM: 16 GB</li>
+      <li>Gráficos: GTX 1080 / RTX 2060 / RX Vega 56</li>
+    </ul>
+  `
+},
+
+undertale: {
+  titulo: "Undertale",
+  descripcion: "Undertale es un juego de rol independiente donde no tienes que matar a nadie. Con un sistema de combate único y múltiples finales, ofrece una experiencia emocional e inolvidable.",
+  imagen: "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2016/08/undertale.jpg?tf=256x",
+  enlace: "https://cuty.io/5G0FG6yeRnd",
+  categoria: "RPG",
+  trailer: "https://www.youtube.com/embed/1Hojv0m3TqA?si=i5DyDE74tjXKAYCo",
+  platafortma: "PC", 
+  requisitos:  `
+    <strong>Requisitos mínimos</strong>
+    <ul>
+      <li>SO: Windows XP/Vista/7/8/10</li>
+      <li>Procesador: 2GHz+</li>
+      <li>RAM: 3 GB</li>
+      <li>Gráficos: 128MB</li>
+      <li>Almacenamiento: 200 MB</li>
+    </ul>
+  `
+},
+
+terraria: {
+  titulo: "Terraria",
+  descripcion: "Terraria es un juego de aventura y supervivencia en 2D con construcción y combate. Los jugadores exploran, cavan, luchan y construyen en mundos generados proceduralmente.",
+  imagen: "https://www.gamestorrents.app/wp-content/uploads/2017/10/terraria-.jpg",
+  enlace: "",
+  categoria: "Aventura",
+  trailer: "",
+  platafortma: "PC", 
+  requisitos:  `
+    <strong>Requisitos mínimos</strong>
+    <ul>
+      <li>SO: Windows XP/Vista/7/8/10</li>
+      <li>Procesador: 2.0 GHz</li>
+      <li>RAM: 2.5 GB</li>
+      <li>Gráficos: 128MB Video Memory, Shader Model 2.0+</li>
+      <li>Almacenamiento: 200 MB</li>
+    </ul>
+  `
+},
+
+portal2: {
+  titulo: "Portal 2",
+  descripcion: "Portal 2 es un juego de rompecabezas en primera persona desarrollado por Valve. Combina mecánicas de portales con una narrativa envolvente y humorística, introduciendo nuevos personajes y modos cooperativos.",
+  imagen: "https://upload.wikimedia.org/wikipedia/en/f/f9/Portal2cover.jpg",
+  enlace: "",
+  categoria: "Puzles",
+  trailer: "https://www.youtube.com/embed/ts-j0nFf2e0",
+  plataforma: "PC",
+  requisitos: `
+    <strong>Requisitos mínimos</strong>
+    <ul>
+      <li>SO: Windows 7 / Vista / XP</li>
+      <li>Procesador: 3.0 GHz P4, Dual Core 2.0 o superior</li>
+      <li>RAM: 2 GB</li>
+      <li>Gráficos: 128 MB o más, compatible con Pixel Shader 2.0b (ATI Radeon X800 / NVIDIA GeForce 7600 o superior)</li>
+      <li>DirectX: Versión 9.0c</li>
+      <li>Almacenamiento: 8 GB de espacio disponible</li>
+    </ul>
+  `
+},
+
+
+
+};
